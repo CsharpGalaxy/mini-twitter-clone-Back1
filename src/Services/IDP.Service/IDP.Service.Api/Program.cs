f2c12e3e2d842e96668680e7986e0e803509260a
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-10-31 10:09:04 +0330

    update

--- src/Services/IDP.Service/IDP.Service.Api/Program.cs
+++ src/Services/IDP.Service/IDP.Service.Api/Program.cs
@@ -6,11 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Configuration
-//    .SetBasePath(Directory.GetCurrentDirectory())
-//    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-//    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
-//    .AddEnvironmentVariables();
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
